52ff96d1b641fa6a70dfbb149e89bcd2ec1ac30a
sifuna2002 <sifunadone4704@gmail.com>
2023-01-12 09:47:49 +0300

    complete CRUD

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const postsRoute = require('./routes/posts');
 const app = express();
 const port = 3000;
+require('dotenv').config();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+//connect to database
+const mongoose = require('mongoose');
+
+//connect to database   
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to database');
+}); 
+
+
+app.get('/', (req, res) =>{
+        res.send('Hello World!')
+});
+
+//Middleware
+app.use('/posts', postsRoute);
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
